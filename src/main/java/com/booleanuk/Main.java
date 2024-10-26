40016702c71f63ed61eb84ca59397ff567ad46e2
LudwigJL <ludwig.j.lundborg@gmail.com>
2024-10-26 18:26:58 +0200

    Added logic for isTriple and Illegitimate characters

---
+++ src/main/java/com/booleanuk/Main.java
@@ -0,0 +1,8 @@
+package com.booleanuk;
+class Main{
+public static void main(String[] args) {
+    String word = "d[o]g";
+
+    Scrabble scrabble = new Scrabble(word);
+    }
+}
--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,14 +1,64 @@
 package com.booleanuk;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 
 public class Scrabble {
+    HashMap<Character, Integer> letterValues = new HashMap<>();
+    Boolean isDouble = false;
+    Boolean isTriple = false;
+
     int score = 0;
 
-    public Scrabble(String word) {
 
-        HashMap<Character, Integer> letterValues = new HashMap<>();
+    public Scrabble(String word) {
+        setLetterValues();
         word = word.toUpperCase();
+        recur(word);
+    }
+
+    public void recur(String s) {
+        if (s.equals("")) {
+            if (isTriple){
+                this.score = 0;
+                System.out.println("Score: " + score);
+            }
+            else {
+                System.out.println("Score: " + score);
+            }
+        }
+
+        else if (!letterValues.containsKey(s.charAt(0))){
+            this.score = 0;
+            System.out.println("Score: " + score);
+        }
+
+        else {
+            if (s.charAt(0) == '[') {
+                isTriple = true;
+            }
+            if (s.charAt(0) == ']') {
+                isTriple = false;
+            }
+            int currScore = getValue(s.charAt(0));
+
+            if (isTriple){
+                this.score += currScore * 3;
+            } else {
+                this.score += currScore;
+            }
+
+            recur (s.substring(1));
+        }
+    }
+
+    public void setLetterValues(){
+
+        //Game vals
+        letterValues.put('[', 0);
+        letterValues.put(']', 0);
+        letterValues.put('{', 0);
+        letterValues.put('}', 0);
 
         // A, E, I, O, U, L, N, R, S, T = 1
         letterValues.put('A', 1);
@@ -23,19 +73,16 @@ public class Scrabble {
         letterValues.put('T', 1);
 
         //D, G = 2
-
         letterValues.put('D', 2);
         letterValues.put('G', 2);
 
         // B, C, M, P = 3
-
         letterValues.put('B', 3);
         letterValues.put('C', 3);
         letterValues.put('M', 3);
         letterValues.put('P', 3);
 
         //F, H, V, W, Y = 4
-
         letterValues.put('F', 4);
         letterValues.put('H', 4);
         letterValues.put('V', 4);
@@ -43,27 +90,23 @@ public class Scrabble {
         letterValues.put('Y', 4);
 
         //K = 5
-
         letterValues.put('K', 5);
 
         //J, X = 8
-
         letterValues.put('J', 8);
         letterValues.put('X', 8);
 
         //Q, Z = 10
-
         letterValues.put('Q', 10);
         letterValues.put('Z', 10);
+    }
 
-        for(int i = 0; i < word.length(); i++){
-            if(letterValues.containsKey(word.charAt(i))){
-                this.score += letterValues.get(word.charAt(i));
-            }
-        }
+    public int getValue(Character c){
+        return letterValues.get(c);
     }
 
     public int score() {
         return this.score;
     }
 }
+
